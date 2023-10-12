2a466c7a0569c7e9d051caae3d05cd4fe4449537
MohammadAzhdari <mohammad.76.az@gmail.com>
2023-10-12 18:22:11 +0330

    first

---
+++ network_dash/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(ILogger<AuthController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
